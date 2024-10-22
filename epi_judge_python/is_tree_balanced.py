b0d79c8b25f4918f67d1fe2960b832102843b88f
Tuan Nguyen <atnhan@gmail.com>
2024-10-22 15:40:43 -0400

    Check balanced trees (recursive)

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,10 +1,25 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+import collections
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    Result = collections.namedtuple('Result', ('is_balanced', 'height'))    
+    
+    def is_balanced_binary_tree_helper(node) -> Result:
+        if not node:
+            return Result(True, 0)
+        left_res = is_balanced_binary_tree_helper(node.left)
+        if not left_res.is_balanced:
+            return Result(False, None)
+        right_res = is_balanced_binary_tree_helper(node.right)
+        if not right_res.is_balanced:
+            return Result(False, None)
+        is_balanced = abs(left_res.height - right_res.height) <= 1
+        height = max(left_res.height, right_res.height) + 1
+        return Result(is_balanced, height)
+
+    return is_balanced_binary_tree_helper(tree).is_balanced
 
 
 if __name__ == '__main__':
