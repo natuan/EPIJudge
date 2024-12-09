16f70282324e58b9c2bf34681cbb68da0641626d
Tuan Nguyen <atnhan@gmail.com>
2024-12-09 16:15:17 -0500

    Problem 9.5: sum from root to leaf in binary tree

--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -3,8 +3,21 @@ from test_framework import generic_test
 
 
 def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
-    # TODO - you fill in here.
-    return 0
+    
+    def sum_root_to_leaf_helper(node, partial_sum = 0) -> int:
+        if not node:
+            # If one of the left or right is None, we return 0, instead of partial sum.
+            # Otherwise, we will over-sum
+            return 0
+        partial_sum = 2 * partial_sum + node.data
+        if not node.left and not node.right:
+            return partial_sum
+        left_sum = sum_root_to_leaf_helper(node.left, partial_sum=partial_sum)
+        right_sum = sum_root_to_leaf_helper(node.right, partial_sum=partial_sum)
+
+        return left_sum + right_sum
+
+    return sum_root_to_leaf_helper(tree)
 
 
 if __name__ == '__main__':
