75b4947449958a75dd88bf66281b2711c16e2ba9
Tuan Nguyen <atnhan@gmail.com>
2024-12-09 19:36:41 -0500

    Problem 9.7: In-order traversal without recursion

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -1,3 +1,4 @@
+import collections
 from typing import List
 
 from binary_tree_node import BinaryTreeNode
@@ -5,9 +6,31 @@ from test_framework import generic_test
 
 
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    if not tree:
+        return []
+    
+    # A node is ready to processed (i.e., to print out its data) if 
+    # its left sub-tree has been processed.
+    NodeInfo = collections.namedtuple("NodeInfo", ("node", "is_ready"))
+    
+    nodes: List[NodeInfo] = [NodeInfo(node=tree, is_ready=False)]
+    results = []
+    while nodes:
+        head = nodes.pop()
+        if head.is_ready:
+            results.append(head.node.data)
+        else:
+            if head.node.right:
+                nodes.append(NodeInfo(node=head.node.right, is_ready=False))
+            
+            # Add this head node into the stack again, but this time with is_ready = True
+            # because we will add its left sub-tree on top of it. Therefore, when we reach
+            # this head node the next time, its left sub-tree will have been processed.
+            nodes.append(NodeInfo(node=head.node, is_ready=True))
 
+            if head.node.left:
+                nodes.append(NodeInfo(node=head.node.left, is_ready=False))
+    return results
 
 if __name__ == '__main__':
     exit(
