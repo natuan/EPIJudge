f739ab9d48324694492609d4f5882ed0cd39fbc6
Tuan Nguyen <atnhan@gmail.com>
2024-12-13 15:06:47 -0500

    Problem  9.12: reconstruct tree from preorder with markers

--- epi_judge_python/tree_from_preorder_with_null.py
+++ epi_judge_python/tree_from_preorder_with_null.py
@@ -7,8 +7,38 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def reconstruct_preorder(preorder: List[int]) -> BinaryTreeNode:
-    # TODO - you fill in here.
-    return BinaryTreeNode()
+
+    def compute_tree_size(root_index_in_preorder: int) -> int:
+        """
+        Compute tree size, including null nodes, for the tree at the given root index
+        """
+        if not preorder[root_index_in_preorder]:
+            return 1  # null root is counted!
+        
+        left_tree_start = root_index_in_preorder + 1
+        left_tree_size = compute_tree_size(left_tree_start) if left_tree_start < len(preorder) else 0
+
+        right_tree_start = root_index_in_preorder + left_tree_size + 1
+        right_tree_size = compute_tree_size(right_tree_start) if right_tree_start < len(preorder) else 0
+
+        return 1 + left_tree_size + right_tree_size
+
+    def reconstruct_preorder_helper(start, end) -> BinaryTreeNode:
+        if start >= end:
+            return None
+        root = BinaryTreeNode(preorder[start]) if preorder[start] else None
+        if root:
+            left_tree_start = start + 1
+            left_tree_size = compute_tree_size(left_tree_start)
+            root.left = reconstruct_preorder_helper(left_tree_start, left_tree_start + left_tree_size)
+
+            right_tree_start = left_tree_start + left_tree_size
+            right_tree_size = compute_tree_size(right_tree_start)
+            root.right = reconstruct_preorder_helper(right_tree_start, right_tree_start + right_tree_size)
+
+        return root
+
+    return reconstruct_preorder_helper(0, len(preorder))
 
 
 @enable_executor_hook
