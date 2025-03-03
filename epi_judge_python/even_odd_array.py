257f2febf65d526609a81437febf1e9ca5e0b567
Tuan Nguyen <atnhan@gmail.com>
2025-03-03 08:59:59 -0500

    Even/odd numbers in array

--- epi_judge_python/even_odd_array.py
+++ epi_judge_python/even_odd_array.py
@@ -9,6 +9,19 @@ from test_framework.test_utils import enable_executor_hook
 
 def even_odd(A: List[int]) -> None:
     # TODO - you fill in here.
+    # Organize elements of array A in the order of even and odd numbers
+    i, j = 0, len(A) - 1
+    while i < j:
+        if A[i] % 2 == 0:
+            i += 1
+        elif A[j] % 2 != 0:
+            j -= 1
+        else:
+            tmp = A[i]
+            A[i] = A[j]
+            A[j] = tmp
+            i += 1
+            j -= 1
     return
 
 
