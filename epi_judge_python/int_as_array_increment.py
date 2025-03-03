4716de9f967b8998f65a85d6ff29da60de089910
Tuan Nguyen <atnhan@gmail.com>
2025-03-03 12:37:42 -0500

    Problem 5.2: increment integer represented as array

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -1,3 +1,6 @@
+"""
+Problem 5.2: increment an arbitrary-large integer represented as an array
+"""
 from typing import List
 
 from test_framework import generic_test
@@ -5,7 +8,23 @@ from test_framework import generic_test
 
 def plus_one(A: List[int]) -> List[int]:
     # TODO - you fill in here.
-    return []
+
+    if not A:
+        return []
+    A.reverse()
+    i, carry = 0, 1
+    while i < len(A) and carry == 1:
+        if A[i] < 9:
+            A[i] += 1
+            carry = 0
+        else:
+            A[i] = 0
+        i += 1
+    if carry == 1:
+        A.append(1)
+    A.reverse()
+
+    return A
 
 
 if __name__ == '__main__':
