4c372033ea96fc639ec17cc665951302c7aae1eb
Tuan Nguyen <atnhan@gmail.com>
2025-03-03 14:12:56 -0500

    Problem 5.4: check if able to reach the end of array from elements' offsets

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -1,3 +1,6 @@
+"""
+Problem 5.4: check if we could reach end of array from elements' max steps
+"""
 from typing import List
 
 from test_framework import generic_test
@@ -5,7 +8,18 @@ from test_framework import generic_test
 
 def can_reach_end(A: List[int]) -> bool:
     # TODO - you fill in here.
-    return True
+    if not A:
+        return False
+    furthest_pos = 0
+    for i in range(len(A)):
+        if furthest_pos < i:
+            # We must be able to reach this position i-th
+            return False
+        furthest_pos = max(furthest_pos, i + A[i])
+        if furthest_pos >= len(A) - 1:
+            # We're able to reach the end!
+            return True
+    return False
 
 
 if __name__ == '__main__':
