234cdb3e44683d25f0cf59f26674a807cc5401de
Tuan Nguyen <atnhan@gmail.com>
2025-03-04 16:52:21 -0500

    Problem 5.9: enumerate prime numbers

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -2,11 +2,21 @@ from typing import List
 
 from test_framework import generic_test
 
+import collections
 
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
     # TODO - you fill in here.
-    return []
+    if n <= 1:
+        return []
+    primes = []
+    excluded = [False] * (n - 1)  # [2, n]
+    for i in range(2, n + 1):
+        if not excluded[i - 2]:
+            primes.append(i)
+            for j in range(2, int(n//i) + 1):
+                excluded[i * j - 2] = True
+    return primes
 
 
 if __name__ == '__main__':
