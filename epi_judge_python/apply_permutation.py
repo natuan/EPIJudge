b6148ba860c70f481585db041070ac8d06a68dee
Tuan Nguyen <atnhan@gmail.com>
2025-03-06 11:59:58 -0500

    Problem 5.10: apply permutation to an array

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -2,9 +2,19 @@ from typing import List
 
 from test_framework import generic_test
 
+# Problem 5.10: apply permutation to array
+
 
 def apply_permutation(perm: List[int], A: List[int]) -> None:
     # TODO - you fill in here.
+    if not A or not perm:
+        return
+    for i in range(len(A)):
+        while perm[i] != i:
+            A[i], A[perm[i]] = A[perm[i]], A[i]
+            
+            # This won't work!!!!: perm[i], perm[perm[i]] = perm[perm[i]], perm[i]
+            perm[perm[i]], perm[i] = perm[i], perm[perm[i]]
     return
 
 
