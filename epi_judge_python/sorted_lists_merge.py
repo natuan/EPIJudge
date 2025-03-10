9d72f453577886dc2eb6896df6f23b051fd34e75
Tuan Nguyen <atnhan@gmail.com>
2025-03-10 14:32:10 -0400

    Problem 7.1: 2nd try to review

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -9,6 +9,31 @@ def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
     # TODO - you fill in here.
 
+    # 2nd try to review
+    if not L1:
+        return L2
+    if not L2:
+        return L1
+    data = L1.data if L1.data < L2.data else L2.data
+    head = ListNode(data, None)
+    tail = head
+    if L1.data < L2.data:
+        L1 = L1.next
+    else:
+        L2 = L2.next
+    while L1 and L2:
+        data = L1.data if L1.data < L2.data else L2.data
+        tail.next = ListNode(data)
+        tail = tail.next
+        if L1.data < L2.data:
+            L1 = L1.next
+        else:
+            L2 = L2.next
+    remaining = L1 if L1 else L2
+    tail.next = remaining
+    return head
+    
+    """
     # See the book for a simpler implementation!
     if not L1:
         return L2
@@ -37,6 +62,7 @@ def merge_two_sorted_lists(L1: Optional[ListNode],
         remaining = remaining.next
 
     return head
+    """
 
 
 if __name__ == '__main__':
