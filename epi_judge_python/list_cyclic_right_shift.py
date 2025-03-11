d607d147e373f8c87c0b394e385d346c6c1e0a02
Tuan Nguyen <atnhan@gmail.com>
2025-03-11 16:04:07 -0400

    Problem 7.9: right shift a list

--- epi_judge_python/list_cyclic_right_shift.py
+++ epi_judge_python/list_cyclic_right_shift.py
@@ -3,10 +3,40 @@ from typing import Optional
 from list_node import ListNode
 from test_framework import generic_test
 
+# Problem 7.9: right shift a list
 
 def cyclically_right_shift_list(L: ListNode, k: int) -> Optional[ListNode]:
     # TODO - you fill in here.
-    return None
+    
+    def _get_len(head):
+        length = 0
+        it = head
+        while it:
+            length += 1
+            it = it.next
+        return length
+    
+    if not L:
+        return None
+      
+    len = _get_len(L)
+    
+    k %= len
+    if k <= 0:
+        return L
+
+    dummy_head = ListNode(0, L)
+    second = dummy_head
+    for _ in range(k):
+        second = second.next
+    first = dummy_head
+    while second.next:
+        first, second = first.next, second.next
+    new_head = first.next
+    first.next = None
+    second.next = L
+    
+    return new_head
 
 
 if __name__ == '__main__':
