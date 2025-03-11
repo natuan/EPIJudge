164415d62e0b0b91ef5361b63266347b6032fbcb
Tuan Nguyen <atnhan@gmail.com>
2025-03-11 15:43:36 -0400

    Problem 7.8: remove duplicate nodes in sorted list

--- epi_judge_python/remove_duplicates_from_sorted_list.py
+++ epi_judge_python/remove_duplicates_from_sorted_list.py
@@ -3,10 +3,21 @@ from typing import Optional
 from list_node import ListNode
 from test_framework import generic_test
 
+# Problem 7.8: remove duplicate nodes from a sorted list
 
 def remove_duplicates(L: ListNode) -> Optional[ListNode]:
     # TODO - you fill in here.
-    return None
+    if not L:
+        return None
+    cur = L
+    while cur:
+        it = cur
+        while it and it.data == cur.data:
+            it = it.next
+        # Property: "it" is None or "it.data" != "cur.data"
+        cur.next = it
+        cur = cur.next
+    return L
 
 
 if __name__ == '__main__':
