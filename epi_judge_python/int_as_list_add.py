41a3660f82106b808941ecd67625d9b7660b5bd1
Tuan Nguyen <atnhan@gmail.com>
2025-03-13 14:06:32 -0400

    Problem 7.13: add list-based integers (2nd version)

--- epi_judge_python/int_as_list_add.py
+++ epi_judge_python/int_as_list_add.py
@@ -8,6 +8,26 @@ from test_framework import generic_test
 
 def add_two_numbers(L1: ListNode, L2: ListNode) -> Optional[ListNode]:
     # TODO - you fill in here.
+    """
+    My second version
+    """
+    result_dummy_head = ListNode()
+    l1_it, l2_it, result_it = L1, L2, result_dummy_head
+    carry = 0
+    while l1_it or l2_it or carry:
+        d1 = l1_it.data if l1_it else 0
+        d2 = l2_it.data if l2_it else 0
+        sum = d1 + d2 + carry
+        result_it.next = ListNode(sum % 10)
+        carry = 1 if sum >= 10 else 0
+        result_it = result_it.next
+        l1_it = l1_it.next if l1_it else None
+        l2_it = l2_it.next if l2_it else None
+
+    return result_dummy_head.next
+
+    """"
+    # My first version -- could be simplified
     if not L1:
         return L2
     if not L2:
@@ -29,7 +49,7 @@ def add_two_numbers(L1: ListNode, L2: ListNode) -> Optional[ListNode]:
         it.next = ListNode(1)
 
     return result_dummy_head.next
-
+    """
 
 if __name__ == '__main__':
     exit(
