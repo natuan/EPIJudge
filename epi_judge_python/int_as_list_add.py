fe1e2334582d894c445846e0bf12c866c56b4498
Tuan Nguyen <atnhan@gmail.com>
2025-03-13 13:56:13 -0400

    Problem 7.13: add list-based integers

--- epi_judge_python/int_as_list_add.py
+++ epi_judge_python/int_as_list_add.py
@@ -3,10 +3,32 @@ from typing import Optional
 from list_node import ListNode
 from test_framework import generic_test
 
+# Problem 7.13: add list-based integers
+# See solution for much simpler implementation
 
 def add_two_numbers(L1: ListNode, L2: ListNode) -> Optional[ListNode]:
     # TODO - you fill in here.
-    return None
+    if not L1:
+        return L2
+    if not L2:
+        return L1
+    
+    it = result_dummy_head = ListNode()
+    l1_it, l2_it = L1, L2
+    carry = 0
+    while l1_it and l2_it:
+        it.next = ListNode((l1_it.data + l2_it.data + carry) % 10)
+        carry = (l1_it.data + l2_it.data + carry) // 10
+        it, l1_it, l2_it = it.next, l1_it.next, l2_it.next
+    remain_it = l1_it if not l2_it else l2_it
+    while remain_it:
+        it.next = ListNode((remain_it.data + carry) % 10)
+        carry = (remain_it.data + carry) // 10
+        it, remain_it = it.next, remain_it.next
+    if carry:
+        it.next = ListNode(1)
+
+    return result_dummy_head.next
 
 
 if __name__ == '__main__':
