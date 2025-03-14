d8a31ee327ca5d080f81eae2f5138753bac4289d
Tuan Nguyen <atnhan@gmail.com>
2025-03-14 18:34:07 -0400

    Problem 5.11: find next permutation

--- epi_judge_python/next_permutation.py
+++ epi_judge_python/next_permutation.py
@@ -2,9 +2,26 @@ from typing import List
 
 from test_framework import generic_test
 
+# Problem 5.11: generate next permutation
 
 def next_permutation(perm: List[int]) -> List[int]:
     # TODO - you fill in here.
+    if not perm:
+        return None
+
+    for i in reversed(range(len(perm)-1)):
+        if perm[i] < perm[i+1]:
+            # perm[i+1:]: decreasing array
+
+            # Find k in [i+1, len) s.t. perm[k] > perm[i] > perm[k+1]
+            # We then swap perm[i] and perm[k], then sort perm[i+1:] increasingly
+            for k in reversed(range(i+1, len(perm))):
+                if perm[k] > perm[i]:
+                    perm[k], perm[i] = perm[i], perm[k]
+                    break
+            perm[i+1:] = reversed(perm[i+1:])
+            return perm
+
     return []
 
 
