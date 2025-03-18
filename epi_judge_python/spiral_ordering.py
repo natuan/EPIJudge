d845bd73f47d56af7f0fe1c37be7e2d3c274f134
Tuan Nguyen <atnhan@gmail.com>
2025-03-18 13:39:37 -0400

    Problem 5.18: spiral ordering of a matrix

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -5,7 +5,20 @@ from test_framework import generic_test
 
 def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
     # TODO - you fill in here.
-    return []
+    if not square_matrix:
+        return []
+
+    n = len(square_matrix)
+    if n == 1:
+        return [square_matrix[0][0]]
+    border = square_matrix[0] + \
+        [square_matrix[i][n-1] for i in range(1, n-1)] + \
+        list(reversed(square_matrix[n-1])) + \
+        [square_matrix[i][0] for i in range(n-2, 0, -1)]
+    
+    sub_matrix = [[square_matrix[r][c] for c in range(1, n-1)] for r in range(1, n-1)]
+
+    return border + matrix_in_spiral_order(sub_matrix)
 
 
 if __name__ == '__main__':
