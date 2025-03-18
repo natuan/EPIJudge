b6fcce0ddbe531cc06c2276bed32f5e1ff4209c3
Tuan Nguyen <atnhan@gmail.com>
2025-03-18 14:52:22 -0400

    Problem 5.20: Pascal triangle

--- epi_judge_python/pascal_triangle.py
+++ epi_judge_python/pascal_triangle.py
@@ -2,10 +2,17 @@ from typing import List
 
 from test_framework import generic_test
 
+# Problem 5.20: Pascal triangle
 
 def generate_pascal_triangle(n: int) -> List[List[int]]:
     # TODO - you fill in here.
-    return []
+    if n <= 0:
+        return []
+    results = [[1] * (i + 1) for i in range(n)]
+    for i in range(1, n):
+        for j in range(1, i):
+            results[i][j] = results[i-1][j-1] + results[i-1][j]
+    return results
 
 
 if __name__ == '__main__':
